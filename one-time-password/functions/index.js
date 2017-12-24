96bb30f1d588cf0e4f402a56db4357e68b4ed136
Sean King <kingsc@miamioh.edu>
2017-12-23 18:23:44 -0600

    feat(Firebase): Add local firebase project

---
+++ one-time-password/functions/index.js
@@ -0,0 +1,11 @@
+const admin = require('firebase-admin')
+const functions = require('firebase-functions')
+const serviceAccount = require('./service_account.json')
+const createUser = require('./create_user')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://rn-swipe.firebaseio.com'
+})
+
+exports.createUser = functions.https.onRequest(createUser)
