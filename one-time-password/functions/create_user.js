c5c8c2f0272c15c74b5aa09e425552600cd15a1f
Sean King <kingsc@miamioh.edu>
2017-12-23 18:42:22 -0600

    feat(GCF): Add create_user function

--- one-time-password/functions/create_user.js
+++ one-time-password/functions/create_user.js
@@ -1,3 +1,13 @@
+const admin = require('firebase-admin')
+
 module.exports = (req, res) => {
-  res.send(req.body)
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'Bad input' })
+  }
+
+  const phone = String(req.body.phone).replace(/[^\d]/g, '')
+
+  admin.auth().createUser({ uid: phone })
+    .then(user => res.send(user))
+    .catch(err => res.status(422).send({ error: err }))
 }
